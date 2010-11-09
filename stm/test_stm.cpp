271f4c7b28a3e2a3b254d28e768fb9f656e39dc9
hack99 <hack99@gmail.com>
2010-11-09 14:10:59 +0800

    Fix STM

--- stm/test_stm.cpp
+++ stm/test_stm.cpp
@@ -13,13 +13,16 @@
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
-#include <boost/stm.hpp>
 #include <boost/threadpool.hpp>
+#include <boost/stm/transaction.hpp>
+#include <boost/stm/tx/pointer.hpp>
 #include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
 
 using namespace std;
 using namespace boost::threadpool;
 using namespace boost::stm;
+using namespace boost;
 
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
@@ -29,152 +32,255 @@ const int thread_count = 8;
 pool tp(thread_count);
 boost::barrier bar(thread_count);
 
+boost::mutex the_mutex;
+
 void init()
 {
 	boost::stm::transaction::initialize_thread();
 	bar.wait();
 }
 
-class object_base : public transaction_object<object_base>
+class object_base
 {
 public:
 	object_base() : id_(0)
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "object_base::object_base()" << endl;
 	}
 
 	virtual ~object_base()
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "object_base::~object_base()" << endl;
 	}
 
 	virtual void write_id(int id)
 	{
-		id_ = id;
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			tx->w(id_) = id;
+		}
 	}
 
 	virtual int read_id() const
 	{
-		return id_;
+		int result = 0;
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			result = tx->r(id_);
+		}
+		return result;
 	}
 
 	virtual void print_id() const
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "id=" << id_ << endl;
 	}
 protected:
-	int id_;
+	native_trans<int> id_;
 };
 
-class test_parent : public transaction_object<test_parent, object_base>
+class manager_item : public transaction_object<manager_item>
+{
+	friend class object_manager;
+public:
+	manager_item() : internal_(NULL)
+	{
+	}
+	virtual ~manager_item()
+	{
+		if (internal_)
+			delete internal_;
+	}
+protected:
+
+	object_base* internal_;
+};
+
+class object_manager
+{
+public:
+	object_manager() : item_(NULL)
+	{
+	}
+
+	object_base* get_obj()
+	{
+		object_base* tmp = NULL;
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			tmp = tx->read_ptr(item_)->internal_;
+		}
+		return tmp;
+	}
+	void add_obj(object_base* obj)
+	{
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			item_ = tx->new_memory(item_);
+			tx->write_ptr(item_)->internal_ = obj;
+		}
+	}
+	void del_obj()
+	{
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			tx->delete_memory(*item_);
+			item_ = NULL;
+		}
+	}
+protected:
+	manager_item *item_;
+};
+
+class test_parent : public object_base
 {
 public:
 	test_parent() : parent_id_(0)
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "test_parent::test_parent()" << endl;
 	}
 
 	virtual ~test_parent()
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "test_parent::~test_parent()" << endl;
 	}
 
 	virtual void write_id(int id)
 	{
-		parent_id_ = id;
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			tx->w(parent_id_) = id;
+		}
 	}
 
 	virtual int read_id() const
 	{
-		return parent_id_;
+		int result = 0;
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			result = tx->r(parent_id_);
+		}
+		return result;
 	}
 
 	virtual void print_id() const
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "id=" << id_ << ":" << parent_id_ << endl;
 	}
 protected:
-	int parent_id_;
+	native_trans<int> parent_id_;
 };
 
-class test_child : public transaction_object<test_child, test_parent>
+class test_child : public test_parent
 {
 public:
 	test_child() : child_id_(0)
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "test_child::test_child()" << endl;
 	}
 
 	virtual ~test_child()
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "test_child::~test_child()" << endl;
 	}
 
 	virtual void write_id(int id)
 	{
-		child_id_ = id;
+		if (id-1 != child_id_)
+		{
+			boost::mutex::scoped_lock lock(the_mutex);
+			cout << child_id_ << ":" << id << endl;
+		}
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			tx->w(child_id_) = id;
+		}
 	}
 
 	virtual int read_id() const
 	{
-		return child_id_;
+		int result = 0;
+        transaction* tx=current_transaction();
+        if (tx!=0) {
+			result = tx->r(child_id_);
+		}
+		return result;
 	}
 
 	virtual void print_id() const
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "id=" << id_ << ":" << parent_id_ << ":" << child_id_ << endl;
 	}
 protected:
-	int child_id_;
+	native_trans<int> child_id_;
 };
 
-object_base* obj = NULL;
+object_manager om;
 
 static void create()
 {
-	atomic(t)
+	try_atomic(t)
 	{
-		obj = t.new_memory<test_child>(NULL);
+		object_base* obj = new test_child;
+		om.add_obj(obj);
 	}
 	before_retry
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "create" << endl;
 	}
 }
 
 static void change()
 {
+
 	try_atomic(t)
 	{
-		object_base* tmp = t.write_ptr(obj);
-		for (int i = 0; i < 10; i++)
-		{
-			tmp->write_id(tmp->read_id()+1);
-		}
+		object_base* obj = om.get_obj();
+		int tmp = obj->read_id();
+		obj->write_id(tmp+1);
 	}
 	before_retry
 	{
+		boost::mutex::scoped_lock lock(the_mutex);
 		cout << "retry change" << endl;
 	}
+
 }
 
 static void print()
 {
-	atomic(t)
+	int my_id = 0;
+
+	try_atomic(t)
 	{
-		obj->print_id();
+		object_base* obj = om.get_obj();
+		my_id = obj->read_id();
 	}
 	before_retry
 	{
-		cout << "print" << endl;
+		boost::mutex::scoped_lock lock(the_mutex);
+		cout << "retry print" << endl;
 	}
+
+	boost::mutex::scoped_lock lock(the_mutex);
+	cout << "id=" << my_id << endl;
 }
 
 static void destroy()
 {
-	atomic(t)
+	try_atomic(t)
 	{
-		t.delete_memory(*obj);
+		om.del_obj();
 	}
 	before_retry
 	{
@@ -203,17 +309,17 @@ int main()
 	for (int i = 0; i < 1000; i++)
 	{
 		tp.schedule(change);
+		tp.schedule(print);
 	}	
 
 	tp.wait();
-
-	boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 
 	print();
 
 	destroy();
 
+	// Trigger defered delete
+	transaction t;
+
 	return 0;
 }
-
-
